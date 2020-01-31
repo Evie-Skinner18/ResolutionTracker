1f90270dd07a4de9eab5710409efb3c3cd7e0b99
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-01-31 18:01:33 +0000

    Made an interface called IResolutionService and gave it some methods

---
+++ ResolutionTracker.Data/Models/Common/IResolutionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ResolutionTracker.Data.Common
+{
+    public interface IResolutionService
+    {
+        IEnumerable<Resolution> GetAllResolutions();
+        Resolution GetResolutionById(int id);
+        void AddResolution(Resolution newResolution);
+
+        string GetTitle(int id);
+        string GetDescription(int id);
+        DateTime GetDeadline(int id);
+        DateTime GetDateCompleted(int id);
+        int GetPercentageComplete(int id);
+    }
+}
