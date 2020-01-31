c44b8e4c3d7004555e1ed2bdbec1aac381ebb2b0
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-01-31 18:05:17 +0000

    Created ResolutionService which will implement the interface

--- ResolutionTracker.Services/ResolutionService.cs
+++ ResolutionTracker.Services/ResolutionService.cs
@@ -1,8 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ResolutionTracker.Data;
+using ResolutionTracker.Data.Common;
 
 namespace ResolutionTracker.Services
 {
-    public class Class1
+    public class ResolutionService : IResolutionService
     {
+        private ResolutionTrackerContext _resolutionTrackerContext;
+
+        public ResolutionService(ResolutionTrackerContext resolutionTrackerContext)
+        {
+            _resolutionTrackerContext = resolutionTrackerContext;
+        }
+
+        public void AddResolution(Resolution newResolution)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Resolution> GetAllResolutions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetDateCompleted(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetDeadline(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDescription(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetPercentageComplete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resolution GetResolutionById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTitle(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
