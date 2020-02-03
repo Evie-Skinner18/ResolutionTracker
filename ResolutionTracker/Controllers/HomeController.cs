797f1baa23851cda198379a5a8f933e1706ea2c6
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-02-03 12:06:24 +0000

    Added a new ResolutionController which depends on the abstract IResolutionService

--- ResolutionTracker/Controllers/HomeController.cs
+++ ResolutionTracker/Controllers/HomeController.cs
@@ -21,7 +21,5 @@ namespace ResolutionTracker.Controllers
         {
             return View();
         }
-
-      
     }
 }
---
+++ ResolutionTracker/Controllers/ResolutionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResolutionTracker.Data.Models.Common;
+
+namespace ResolutionTracker.Controllers
+{
+    public class ResolutionController : Controller
+    {
+        private IResolutionService _resolutionService;
+
+        public ResolutionController(IResolutionService resolutionService)
+        {
+            _resolutionService = resolutionService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
