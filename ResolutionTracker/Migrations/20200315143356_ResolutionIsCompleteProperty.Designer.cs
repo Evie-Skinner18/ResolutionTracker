0869160d636cda55861b24e5a7ff2bd3caebca58
Evie-Skinner18 <ohowkind@gmail.com>
2020-03-15 14:40:32 +0000

    Added new migration for IsComplete property. Updated DB

--- ResolutionTracker/Migrations/20200315143356_ResolutionIsCompleteProperty.Designer.cs
+++ ResolutionTracker/Migrations/20200315143356_ResolutionIsCompleteProperty.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ResolutionTracker.Data;
@@ -9,9 +10,10 @@ using ResolutionTracker.Data;
 namespace ResolutionTracker.Migrations
 {
     [DbContext(typeof(ResolutionTrackerContext))]
-    partial class ResolutionTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20200315143356_ResolutionIsCompleteProperty")]
+    partial class ResolutionIsCompleteProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
